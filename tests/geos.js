7f9f2b776e5c4cb5c8ae014531b58ca203e6a900
Carlos Ferry <cferry@beryldb.com>
2021-10-19 15:29:39 -0300

    Updates.

---
+++ tests/geos.js
@@ -0,0 +1,49 @@
+/*
+ * node-beryl - NodeJS Client for BerylDB.
+ * http://www.beryldb.com
+ *
+ * Copyright (C) 2021 - Carlos F. Ferry <cferry@beryldb.com>
+ * 
+ * This file is part of BerylDB. BerylDB is free software: you can
+ * redistribute it and/or modify it under the terms of the BSD License
+ * version 3.
+ *
+ * More information about our licensing can be found at https://docs.beryl.dev
+ */
+
+"use strict";
+
+var Link = require("../connection");
+
+/* Adds Latitude and Longitude coordinates for Miami */
+
+Link.geoadd("Miami", "25.761681", "-80.191788").then(function(data) 
+{
+        console.log(data);
+})
+.catch(function(error)
+{
+        console.log("error: " + error.message);
+});
+
+/* Adds Latitude and Longitude coordinates for Los Angeles */
+
+Link.geoadd("Los_Angeles", "34.052235", "-118.243683").then(function(data) 
+{
+        console.log(data);
+})
+.catch(function(error)
+{
+        console.log("error: " + error.message);
+});
+
+/* Calculates distance between Miami and Los Angeles */
+
+Link.gcalc("Miami", "Los_Angeles").then(function(data) 
+{
+        console.log(data);
+})
+.catch(function(error)
+{
+        console.log("error: " + error.message);
+});
---
+++ tests/maps.js
@@ -0,0 +1,64 @@
+/*
+ * node-beryl - NodeJS Client for BerylDB.
+ * http://www.beryldb.com
+ *
+ * Copyright (C) 2021 - Carlos F. Ferry <cferry@beryldb.com>
+ * 
+ * This file is part of BerylDB. BerylDB is free software: you can
+ * redistribute it and/or modify it under the terms of the BSD License
+ * version 3.
+ *
+ * More information about our licensing can be found at https://docs.beryl.dev
+ */
+
+"use strict";
+
+var Link = require("../connection");
+
+/* Creates map 'a' with hash 'b' and value 'c'. */
+
+Link.hset("a", "b", "c").then(function(data) 
+{
+        console.log(data);
+})
+.catch(function(error)
+{
+        console.log("error: " + error.message);
+});
+
+/* Retrieves map 'a' and hash 'b' */
+
+Link.hget("a", "b").then(function(data) 
+{
+        console.log(data);
+})
+.catch(function(error)
+{
+        console.log("error: " + error.message);
+});
+
+/* We list all items associated with list 'a' */
+
+Link.hlist("a").then(function(data) 
+{
+        data.forEach(function(item)
+        {
+                console.log(item);
+        })
+})
+.catch(function(error)
+{
+        console.log("error: " + error.message);
+});
+
+/* We remove hash 'b' from map 'a' */
+
+Link.hdel("a", "b").then(function(data) 
+{
+        console.log(data);
+})
+.catch(function(error)
+{
+        console.log("error: " + error.message);
+});
+
