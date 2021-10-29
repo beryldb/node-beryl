df082636a0a3334b220cec2a99df1c2bb9b6a6b7
Carlos Ferry <cferry@beryldb.com>
2021-10-28 23:45:13 -0300

    Fixing expire bug.

---
+++ tests/expires.js
@@ -0,0 +1,45 @@
+/*
+ * node-beryl - NodeJS Client for BerylDB.
+ * http://www.beryldb.com
+ *
+ * Copyright (C) 2021 - Carlos F. Ferry <cferry@beryldb.com>
+ * 
+ * This file is part of BerylDB. BerylDB is free software: you can
+ * redistribute it and/or modify it under the terms of the BSD License
+ * version 3.
+ *
+ * More information about our licensing can be found at https://docs.beryl.dev
+ */
+
+"use strict";
+
+var Link = require("../connection");
+
+/* Defines hello with 'world' */
+
+Link.set("hello", "world").then(function(data) 
+{
+        console.log(data);
+})
+.catch(function(error)
+{
+        console.log("error: " + error.message);
+});
+
+Link.expire("hello", 300).then(function(data) 
+{
+        console.log(data);
+})
+.catch(function(error)
+{
+        console.log("error: " + error.message);
+});
+
+Link.expires("hello").then(function(data) 
+{
+        console.log(data);
+})
+.catch(function(error)
+{
+        console.log("error: " + error.message);
+});
